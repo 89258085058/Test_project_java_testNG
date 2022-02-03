e2de763da5b8f35bd7963900354e035759e8aaa2
89258085058 <86528556+89258085058@users.noreply.github.com>
2022-02-03 20:41:36 +0300

    обавлено кеширование для удаление шрупп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -6,9 +6,7 @@ import org.openqa.selenium.WebElement;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class GroupHelper extends HelpperBAse {
 
@@ -51,12 +49,7 @@ public class GroupHelper extends HelpperBAse {
         click(By.name("update"));
     }
 
-    public void create(GroupData group) {
-        initGroupVreation();
-        fillGruopForm(group);
-        submitGroupCreation();
-        reternToGroupPage();
-    }
+
 
     public boolean isThereGroup() {
         return isElementPresent(By.name("selected[]"));
@@ -66,16 +59,29 @@ public class GroupHelper extends HelpperBAse {
         return wd.findElements(By.name("selected[]")).size();
     }
 
+    private Groups groupCache = null;
 
     public Groups all() {
-        Groups groups = new Groups();
+        if (groupCache != null) {
+            return  new Groups(groupCache);
+        }
+
+        groupCache = new Groups();
         List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
         for (WebElement element : elements) {
             String name = element.getText();
             int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-            groups.add(new GroupData().withId(id).withName(name));
+            groupCache.add(new GroupData().withId(id).withName(name));
         }
-        return groups;
+        return new Groups(groupCache);
+    }
+
+    public void create(GroupData group) {
+        initGroupVreation();
+        fillGruopForm(group);
+        submitGroupCreation();
+        groupCache =  null;
+        reternToGroupPage();
     }
 
     public void modify(GroupData group) {
@@ -83,13 +89,14 @@ public class GroupHelper extends HelpperBAse {
         initGroupModification();
         fillGruopForm(group);
         submitGroupModifivation();
+        groupCache =  null;
         reternToGroupPage();
     }
-
 
     public void delete(GroupData group) {
         selectGroupById(group.getId());
         deleteSelected();
+        groupCache =  null;
         reternToGroupPage();
 
     }
